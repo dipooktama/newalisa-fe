b71ffe6500cd0291dcaa68d7208a599f0c6ab2f0
dipooktama <dipooktama@usu.ac.id>
2025-07-24 13:02:49 +0700

    feat: landing page will show coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,8 @@
+<!--This page should be a landing page-->
 <script lang="ts">
-    import Cards from "$lib/components/Cards.svelte";
-    const cardItems = [
-    {
-      value: "/chats",
-      title: "Obrolan",
-      content:
-        "Berdiskusi dengan ALISA.",
-    },
-    {
-      value: "/chats",
-      title: "Rancangan Mata Kuliah",
-      content:
-        "Membuat rancangan mata kuliah dengan ALISA.",
-    },
-    {
-      value: "/chats",
-      title: "Konten Mata Kuliah",
-      content:
-        "Membuat rancangan konten perkuliahan berbasis permainan atau video.",
-    },
-  ];
+  const text = "COMING SOON"
 </script>
 
-
-<section class="h-screen w-full">
-    <div class="flex flex-row justify-center mt-auto p-4 gap-4">
-        {#each cardItems as item }
-            <Cards href={item.value} title={item.title} description={item.content} />
-        {/each}
-    </div>
+<section class="h-screen w-full flex flex-col justify-center">
+  <h1 class="text-9xl text-center">{text}</h1>
 </section>
